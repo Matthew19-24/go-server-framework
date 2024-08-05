fab349776e971da1a0ee3f372c4324c73160c13d
stitchy <matt.mccaughey91@gmail.com>
2024-08-05 12:29:02 -0400

    add server setup to main.go

--- main.go
+++ main.go
@@ -1,7 +1,15 @@
 package main
 
-import "testServer/db"
+import (
+	"testServer/db"
+	"testServer/routes"
+
+	"github.com/gofiber/fiber/v2"
+)
 
 func main() {
 	db.Connect()
+	server := fiber.New()
+	routes.Setup(server)
+	server.Listen(":" + db.GetPort())
 }
