deb2db17fde6f14663ea161b0cc9e9354d584822
stitchy <matt.mccaughey91@gmail.com>
2024-08-05 11:31:25 -0400

    add models/foreign.go

---
+++ models/foreign.go
@@ -0,0 +1,12 @@
+package models
+
+import(
+	"github.com/google/uuid"
+)
+
+type Foreign struct {
+	ForeignUUID uuid.UUID `json:"foreignUUID" gorm:"primary_key;column:foreign_uuid;type:varchar(36)"`
+	ForeignName string `json:"foreignName" gorm:"column:foreignName;type:varchar(256)"`
+	ForeignExample uuid.UUID `json:"foreignExample" gorm:"column:foreign_example;varchar(36)"`
+	Example Example `json:"-" gorm:"foreignkey:foreign_example"`
+}
